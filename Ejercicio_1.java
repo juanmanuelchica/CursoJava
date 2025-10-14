b19c0df1a0f6912e0b3f583e352ae43d745c57d6
juanmanuelchica <juanito0903h@gmail.com>
2025-10-14 18:16:25 -0500

    Add files via upload

---
+++ Ejercicio_1.java
@@ -0,0 +1,28 @@
+
+package Secuenciales;
+
+/**
+ *
+ * @author Juan Manuel Chicac
+ */
+public class Ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // Crear variables
+        String username="Camila";
+        int cofreUno=8;
+        int cofreDos=12;   
+        int sumaDulces;      
+        
+        //Proceso
+        sumaDulces=cofreUno + cofreDos;
+        
+        //Salida
+        System.out.print(username + " Camila ahora tiene un total de " + sumaDulces + " caramelos magicos."); 
+    }
+    
+}
---
+++ Ejercicios_2.java
@@ -0,0 +1,32 @@
+
+package Secuenciales;
+
+/**
+ *
+ * @author Juan Manuel Chica
+ */
+public class Ejercicios_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     
+      // Crear Variables
+     
+      double Primerreto=3.5;
+      double Segundoreto=4.2;
+      double Tercerreto=2.8;
+      double Solucion;
+      
+      //Proceso
+      Solucion=(Primerreto + Segundoreto + Tercerreto) /3;
+       
+      //Salida
+      System.out.print("Tu promedio final es= "+ Solucion +" ¡Nivel superado!" );
+      
+   
+    }
+    
+}
